fe921853c255c4cbb2936d015125741654f08675
[dyad] <fabiosamurai124@gmail.com>
2026-02-23 21:57:11 +0100

    [dyad] Correção de erro de importação no Tiptap - wrote 1 file(s)

--- src/components/notes/RichTextEditor.tsx
+++ src/components/notes/RichTextEditor.tsx
@@ -2,11 +2,11 @@
 
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
-import Underline from '@tiptap/extension-underline';
-import TextAlign from '@tiptap/extension-text-align';
-import Highlight from '@tiptap/extension-highlight';
-import TextStyle from '@tiptap/extension-text-style';
-import Color from '@tiptap/extension-color';
+import { Underline } from '@tiptap/extension-underline';
+import { TextAlign } from '@tiptap/extension-text-align';
+import { Highlight } from '@tiptap/extension-highlight';
+import { TextStyle } from '@tiptap/extension-text-style';
+import { Color } from '@tiptap/extension-color';
 import { NoteToolbar } from './NoteToolbar';
 import { useEffect } from 'react';
 
